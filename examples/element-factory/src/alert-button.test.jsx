896d9d3bd269627e60021f91904fc2e6e269d8f9
Jaimin Parmar <jaimins100@gmail.com>
2026-09-09 10:11:07 -0700

    alert spy tests

--- examples/element-factory/src/alert-button.test.jsx
+++ examples/element-factory/src/alert-button.test.jsx
@@ -2,13 +2,29 @@ import { render, screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { AlertButton } from './alert-button';
+import React from 'react';
 
-describe.todo('AlertButton', () => {
+describe('AlertButton', () => {
   beforeEach(() => {});
 
   afterEach(() => {});
 
   it('should render an alert button', async () => {});
 
-  it('should trigger an alert', async () => {});
+    it('should trigger an alert', async () => {
+        render(<AlertButton />)
+
+        const alertSpy = vi.spyOn(window, 'alert');
+        const input = screen.getByLabelText('Message')
+        const button = screen.getByRole('button', { name: /trigger alert/i })
+
+        await act(async () => {
+        await userEvent.clear(input)
+        await userEvent.type(input, 'Hello');
+        await userEvent.click(button);
+        });
+
+        expect(alertSpy).toHaveBeenCalled()
+        expect(alertSpy).toHaveBeenCalledWith('Hello')
+  });
 });
