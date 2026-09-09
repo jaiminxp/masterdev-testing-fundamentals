b23c563e29b709d4ca132610b79170e4856f3a9d
Jaimin Parmar <jaimins100@gmail.com>
2026-09-09 12:37:10 +0530

    mocks

--- examples/scratchpad/index.test.js
+++ examples/scratchpad/index.test.js
@@ -1,10 +1,25 @@
 import { test, expect, vi } from 'vitest';
 
-const consolespy = vi.spyOn(console, 'log');
+const logSpy = vi.spyOn(console, 'log');
+const randomSpyMock = vi.spyOn(Math, 'random').mockImplementation(() => 0.5);
 
-test('a super simple test', () => {
-  console.log('hellow owrld');
+test('spy test', () => {
+  console.log('hello world');
 
-  expect(consolespy).toHaveBeenCalled();
-  expect(consolespy).toHaveBeenCalledWith('hellow owrld');
+  expect(logSpy).toHaveBeenCalled();
+  expect(logSpy).toHaveBeenCalledWith('hello world');
+});
+
+test('mock test', () => {
+  const mockFn = vi.fn();
+
+  mockFn('hello world');
+
+  expect(mockFn).toHaveBeenCalled();
+  expect(mockFn).toHaveBeenCalledWith('hello world');
+});
+
+test('spy mockimplementation test', () => {
+  const result = Math.random();
+  expect(result).toBe(0.5);
 });
